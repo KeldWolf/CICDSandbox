ffef0d22dbf47edc5e6da1695f7c076c1e2ac511
BuildTools <unconfigured@null.spigotmc.org>
2020-11-02 12:03:57 -0800

    Added basic Hello World sln for Azure CICD testing

---
+++ Hello World/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
